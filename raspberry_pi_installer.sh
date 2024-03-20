484bdcfd8734a97fbd2aa91a56832dbdb7b8402b
Brian Wallen <bwallen@gmail.com>
2024-03-20 16:55:40 -0400

    Fixes for the Ubuntu installer, added the Raspberry Pi OS installer

--- raspberry_pi_installer.sh
+++ raspberry_pi_installer.sh
@@ -3,9 +3,9 @@
 . /etc/os-release
 . colors.sh
 
-if [ "$NAME" != "Ubuntu" ]
+if [ "$NAME" != "Raspbian GNU/Linux" ] && [ "$NAME" != "Debian GNU/Linux" ]
 then
-  read -p "${YELLOW} This installer is for Ubuntu but this system is $NAME. Do you want to run anyway? [y/N] ${NC}" confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+  read -p "${YELLOW} This installer is for Raspberry Pi OS but this system is $NAME. Do you want to run anyway? [y/N] ${NC}" confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 fi
 
 USERNAME=$(whoami)
@@ -23,8 +23,10 @@ cp iconsets.sqlite ~/ots/ots.db
 echo "${GREEN}Installing packages via apt. You may be prompted for your sudo password...${NC}"
 
 sudo apt update && sudo NEEDRESTART_MODE=a apt upgrade -y
-sudo NEEDRESTART_MODE=a apt install curl python3 python3-pip rabbitmq-server git openssl nginx ffmpeg -y
-sudo pip3 install poetry pyotp lastversion
+sudo NEEDRESTART_MODE=a apt install curl python3 python3-pip rabbitmq-server git openssl nginx ffmpeg python3-pyotp python3-poetry -y
+
+#curl -sSL https://install.python-poetry.org | python3 -
+#pip3 install --user poetry-plugin-expo
 
 if [ -d "/opt/OpenTAKServer" ]; then
   cd /opt/OpenTAKServer || exit
@@ -34,9 +36,20 @@ else
   sudo chown "$USERNAME":"$USERNAME" /opt/OpenTAKServer -R
 fi
 
-poetry config virtualenvs.in-project true
-poetry config virtualenvs.options.system-site-packages true
-cd /opt/OpenTAKServer && poetry update && poetry install
+~/.local/bin/poetry config virtualenvs.in-project true
+~/.local/bin/poetry config virtualenvs.options.system-site-packages false
+
+cd /opt/OpenTAKServer || exit
+.venv/bin/pip3 install cryptography==41.0.7
+#echo "${GREEN} Locking${NC}"
+#~/.local/bin/poetry lock --no-update
+#echo "${GREEN} Exporting${NC}"
+#~/.local/bin/poetry export > requirements.txt
+#echo "${GREEN} Installing ${NC}"
+#.venv/bin/pip3 install -r requirements.txt
+#.venv/bin/pip3 install .
+export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+cd /opt/OpenTAKServer && poetry update && poetry install && poetry run pip3 install lastversion
 
 cd "$INSTALLER_DIR" || exit
 
@@ -125,18 +138,16 @@ cd "$INSTALLER_DIR" || exit
 echo "${GREEN}Installing mediamtx...${NC}"
 mkdir -p ~/ots/mediamtx/recordings
 
-cd ~/ots/mediamtx
+cd /opt/OpenTAKServer
 
-ARCH=$(uname -m)
 KERNEL_BITS=$(getconf LONG_BIT)
-if [ "$ARCH" == "x86_64" ]; then
-  lastversion --filter '~*linux_amd64' --assets download bluenviron/mediamtx
-elif [ "$KERNEL_BITS" == 32 ]; then
-  lastversion --filter '~*linux_armv7' --assets download bluenviron/mediamtx
+if [ "$KERNEL_BITS" == 32 ]; then
+  poetry run lastversion --filter '~*linux_armv7' --assets download bluenviron/mediamtx -o ~/ots/mediamtx
 elif [ "$KERNEL_BITS" == 64 ]; then
-  lastversion --filter '~*linux_arm64v8' --assets download bluenviron/mediamtx
+  poetry run lastversion --filter '~*linux_arm64v8' --assets download bluenviron/mediamtx -o ~/ots/mediamtx
 fi
 
+cd ~/ots/mediamtx
 tar -xf ./*.tar.gz
 cd "$INSTALLER_DIR"
 cp mediamtx.yml ~/ots/mediamtx/
@@ -153,7 +164,6 @@ EOF
 sudo sed -i "s~SERVER_CERT_FILE~${HOME}/ots/ca/certs/opentakserver/opentakserver.pem~g" ~/ots/mediamtx/mediamtx.yml
 sudo sed -i "s~SERVER_KEY_FILE~${HOME}/ots/ca/certs/opentakserver/opentakserver.nopass.key~g" ~/ots/mediamtx/mediamtx.yml
 sudo sed -i "s~OTS_FOLDER~${HOME}/ots~g" ~/ots/mediamtx/mediamtx.yml
-
 
 sudo systemctl daemon-reload
 sudo systemctl enable mediamtx
@@ -180,7 +190,7 @@ Wants=network.target
 [Service]
 User=$(whoami)
 WorkingDirectory=/opt/OpenTAKServer
-ExecStart=/usr/local/bin/poetry run python /opt/OpenTAKServer/opentakserver/app.py
+ExecStart=/opt/OpenTAKServer/.venv/bin/python /opt/OpenTAKServer/opentakserver/app.py
 [Install]
 WantedBy=multi-user.target
 EOF
