9fe7fb571469dc20981379013a2154bf6a32c561
Brian Wallen <bwallen@gmail.com>
2025-01-28 22:37:32 -0500

    Added unit files for cot_parser and eud_handler

--- raspberry_pi_installer.sh
+++ raspberry_pi_installer.sh
@@ -222,9 +222,47 @@ StandardError=append:${HOME}/ots/logs/opentakserver.log
 WantedBy=multi-user.target
 EOF
 
+sudo tee /etc/systemd/system/cot_parser.service >/dev/null << EOF
+[Unit]
+Wants=network.target rabbitmq-server.service
+After=network.target rabbitmq-server.service
+[Service]
+User=$(whoami)
+WorkingDirectory=${HOME}/ots
+ExecStart=${HOME}/.opentakserver_venv/bin/cot_parser
+Restart=on-failure
+RestartSec=5s
+StandardOutput=append:${HOME}/ots/logs/opentakserver.log
+StandardError=append:${HOME}/ots/logs/opentakserver.log
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo tee /etc/systemd/system/eud_handler.service >/dev/null << EOF
+[Unit]
+Wants=network.target rabbitmq-server.service
+After=network.target rabbitmq-server.service
+[Service]
+User=$(whoami)
+WorkingDirectory=${HOME}/ots
+ExecStart=${HOME}/.opentakserver_venv/bin/eud_handler
+Restart=on-failure
+RestartSec=5s
+StandardOutput=append:${HOME}/ots/logs/opentakserver.log
+StandardError=append:${HOME}/ots/logs/opentakserver.log
+[Install]
+WantedBy=multi-user.target
+EOF
+
 sudo systemctl daemon-reload
 sudo systemctl enable opentakserver
 sudo systemctl start opentakserver
+
+sudo systemctl enable cot_parser
+sudo systemctl start cot_parser
+
+sudo systemctl enable eud_handler
+sudo systemctl start eud_handler
 
 echo "${GREEN}Configuring RabbitMQ...${NC}"
 sudo wget https://raw.githubusercontent.com/brian7704/OpenTAKServer-Installer/master/rabbitmq.conf -qO /etc/rabbitmq/rabbitmq.conf
